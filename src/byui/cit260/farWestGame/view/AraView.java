f09cf109de0ce43dec3358c398cb9add52ce69e7
Camarillo-Araceli <aracelicaes@users.noreply.github.com>
2018-06-23 18:16:17 -0500

    new class added

---
+++ src/byui/cit260/farWestGame/view/AraView.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.farWestGame.view;
+
+/**
+ *
+ * @author kitka_000
+ */
+public class AraView {
+    
+}
